9851afa728462b15470c96bee464263d82aa46a5
vigzmv <vigzmv@outlook.com>
2016-11-29 00:35:42 +0530

    beautify

--- js/index.js
+++ js/index.js
@@ -1,85 +1,85 @@
 var $messages = $('.messages-content'),
-d, h, m,
-i = 0;
+    d, h, m,
+    i = 0;
 
 $(window).load(function() {
-	$messages.mCustomScrollbar();
-	setTimeout(function() {
-		fakeMessage();
-	}, 100);
+    $messages.mCustomScrollbar();
+    setTimeout(function() {
+        fakeMessage();
+    }, 100);
 });
 
 function updateScrollbar() {
-	$messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
-		scrollInertia: 10,
-		timeout: 0
-	});
+    $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+        scrollInertia: 10,
+        timeout: 0
+    });
 }
 
-function setDate(){
-	d = new Date()
-	if (m != d.getMinutes()) {
-		m = d.getMinutes();
-		$('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
-	}
+function setDate() {
+    d = new Date()
+    if (m != d.getMinutes()) {
+        m = d.getMinutes();
+        $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    }
 }
 
 function insertMessage() {
-	msg = $('.message-input').val();
-	if ($.trim(msg) == '') {
-		return false;
-	}
-	$('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
-	setDate();
-	$('.message-input').val(null);
-	updateScrollbar();
-	setTimeout(function() {
-		fakeMessage();
-	}, 1000 + (Math.random() * 20) * 100);
+    msg = $('.message-input').val();
+    if ($.trim(msg) == '') {
+        return false;
+    }
+    $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    $('.message-input').val(null);
+    updateScrollbar();
+    setTimeout(function() {
+        fakeMessage();
+    }, 1000 + (Math.random() * 20) * 100);
 }
 
 $('.message-submit').click(function() {
-	insertMessage();
+    insertMessage();
 });
 
 $(window).on('keydown', function(e) {
-	if (e.which == 13) {
-		insertMessage();
-		return false;
-	}
+    if (e.which == 13) {
+        insertMessage();
+        return false;
+    }
 })
 
 var Fake = [
-'Hi there, I\'m BATMAN and you?',
-'Do you wanna know My Secret Identity?',
-'Nice to meet you',
-'How are you?',
-'Not too bad, thanks',
-'What do you do?',
-'That\'s awesome',
-'Codepen is a nice place to stay',
-'I think you\'re a nice person',
-'Why do you think that?',
-'Can you explain?',
-'Anyway I\'ve gotta go now',
-'It was a pleasure chat with you',
-'Bye',
-':)'
+    'Hi there, I\'m BATMAN and you?',
+    'Do you wanna know My Secret Identity?',
+    'Nice to meet you',
+    'How are you?',
+    'Not too bad, thanks',
+    'What do you do?',
+    'That\'s awesome',
+    'Codepen is a nice place to stay',
+    'I think you\'re a nice person',
+    'Why do you think that?',
+    'Can you explain?',
+    'Anyway I\'ve gotta go now',
+    'It was a pleasure chat with you',
+    'Bye',
+    ':)'
 ]
 
 function fakeMessage() {
-	if ($('.message-input').val() != '') {
-		return false;
-	}
-	$('<div class="message loading new"><figure class="avatar"><img src="img/bat.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
-	updateScrollbar();
+    if ($('.message-input').val() != '') {
+        return false;
+    }
+    $('<div class="message loading new"><figure class="avatar"><img src="img/bat.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+    updateScrollbar();
 
-	setTimeout(function() {
-		$('.message.loading').remove();
-		$('<div class="message new"><figure class="avatar"><img src="img/bat.png" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
-		setDate();
-		updateScrollbar();
-		i++;
-	}, 1000 + (Math.random() * 20) * 100);
+    setTimeout(function() {
+        $('.message.loading').remove();
+        $('<div class="message new"><figure class="avatar"><img src="img/bat.png" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
+        setDate();
+        updateScrollbar();
+        i++;
+    }, 1000 + (Math.random() * 20) * 100);
 
 }
